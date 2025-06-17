719ec6900c80909dc77127d05e239c06f3bc28ea
abdoukh12kh <zaaaa.b.d30@gmail.com>
2025-06-18 00:02:53 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,11 +24,11 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(config: CloudstreamExtension.() -> Unit) =
-    extensions.getByName<CloudstreamExtension>("cloudstream").config()
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
-fun Project.android(config: BaseExtension.() -> Unit) =
-    extensions.getByName<BaseExtension>("android").config()
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
@@ -44,7 +44,7 @@ subprojects {
 
         defaultConfig {
             minSdk = 21
-            compileSdk = 34
+            compileSdkVersion(34)
             targetSdk = 34
         }
 
@@ -69,4 +69,14 @@ subprojects {
         val cloudstream by configurations
         val implementation by configurations
 
-        cloudstrea
+        cloudstream("com.lagradost:cloudstream3:pre-release")
+        implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.4.11")
+        implementation("org.jsoup:jsoup:1.18.3")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
